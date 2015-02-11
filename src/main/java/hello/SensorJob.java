6fef3f18ca1a8b0272a6576281b0eb771fd65dba
Ajay Ambre <ajayambre88@gmail.com>
2015-02-11 22:12:30 +0530

    Added JobScheduler

---
+++ src/main/java/hello/SensorJob.java
@@ -0,0 +1,16 @@
+package hello;
+
+import java.util.Date;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class SensorJob implements Job{
+
+	@Override
+	public void execute(JobExecutionContext arg0) throws JobExecutionException {
+		System.out.println("Checking Sensor Status"+new Date());	
+	}
+
+}
--- src/main/java/hello/SensorJobTrigger.java
+++ src/main/java/hello/SensorJobTrigger.java
@@ -3,19 +3,14 @@ package hello;
 import org.quartz.JobBuilder;
 import org.quartz.JobDetail;
 import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
 import org.quartz.SimpleScheduleBuilder;
 import org.quartz.Trigger;
 import org.quartz.TriggerBuilder;
 import org.quartz.impl.StdSchedulerFactory;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan
-@EnableAutoConfiguration
-public class Application {
-	public static void main(String[] args) {
+public class SensorJobTrigger {
+/*
+	public static void main(String[] args) throws Exception{
 		JobDetail job = JobBuilder.newJob(SensorJob.class)
 				.withIdentity("sensorJob", "group1").build();
 
@@ -29,14 +24,8 @@ public class Application {
 						.build();
 
 		// schedule it
-		try {
-			Scheduler scheduler = new StdSchedulerFactory().getScheduler();
-			scheduler.start();
-			scheduler.scheduleJob(job, trigger);
-		} catch (SchedulerException e) {
-			System.out.println("Could not start SensorJobScheduler!!!");
-			e.printStackTrace();
-		}
-        SpringApplication.run(Application.class, args);
-    }
+		Scheduler scheduler = new StdSchedulerFactory().getScheduler();
+		scheduler.start();
+		scheduler.scheduleJob(job, trigger);
+	}*/
 }
